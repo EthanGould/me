884a908b9579b89c047c738cbd217ae4e60c9af4
Ethan Gould <ethan.g.907@gmail.com>
2014-12-22 09:51:19 -0900

    set up basic breakpoints with media queries

--- new-new/scripts/main.js
+++ new-new/scripts/main.js
@@ -1,15 +1,3 @@
 $(document).ready(function() {
 	console.log('scripts running');
-
-	$('.link-to-me').hover(function(){
-		$(this).css('background', '#10a7ed');
-	}, function() {
-		$(this).css('background', '#55c7fc');
-	});
-
-	$('.profile-pic').hover(function() {
-		$(this).css('border', '1px solid lightgrey');
-	}, function() {
-		$(this).css('border', '1px solid black');
-	});
 });

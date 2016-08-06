26153808d8e8ed9f9cb6b79a04337cc67b1136f3
Ethan Gould <ethan.g.907@gmail.com>
2016-08-06 14:47:32 -0400

    profile picture spins on hover

--- new-new/scripts/main.js
+++ new-new/scripts/main.js
@@ -1,12 +1,23 @@
 $(document).ready( function() {
 	
-	$( '.flipup-button' ).mouseenter( function() {
-		$( this ).find( '.flipup-box' ).addClass( 'slideup' );
-		$( this ).find( '.flipup-box' ).removeClass( 'slidedown' );
+	// contact buttons
+	$('.flipup-button').mouseenter(function() {
+		$(this).find('.flipup-box').addClass('slideup');
+		$(this).find('.flipup-box').removeClass('slidedown');
 	});
 
-	$( '.flipup-button' ).mouseleave( function() {
-		$( this ).find( '.flipup-box' ).removeClass( 'slideup');
-		$( this ).find( '.flipup-box' ).addClass( 'slidedown');
+	$('.flipup-button').mouseleave(function() {
+		$(this).find('.flipup-box').removeClass('slideup');
+		$(this).find('.flipup-box').addClass('slidedown');
 	});
+
+	// spinning profile picture
+	$('.site-header__img').mouseover(function() {
+		var $this = $(this);
+		$this.addClass('spin');
+		setTimeout(function() {
+			$this.removeClass('spin');
+		}, 500);
+	});
+
 });

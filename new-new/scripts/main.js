ab7eaec5b7fafd1e7d534f4bcde371f81523865d
Ethan Gould <ethan.g.907@gmail.com>
2016-08-14 14:12:02 -0400

    removed JS, anomations redone in CSS

--- new-new/scripts/main.js
+++ new-new/scripts/main.js
@@ -1,13 +1,1 @@
-$(document).ready( function() {
-	
-	// contact buttons
-	$('.flipup-button').mouseenter(function() {
-		$(this).find('.flipup-box').addClass('slideup');
-		$(this).find('.flipup-box').removeClass('slidedown');
-	});
-
-	$('.flipup-button').mouseleave(function() {
-		$(this).find('.flipup-box').removeClass('slideup');
-		$(this).find('.flipup-box').addClass('slidedown');
-	});
-});
+// nothing to do yet

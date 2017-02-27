7ffa04cdc80e749a0a4d7a2331c2b8fd59a331c4
Ethan Gould <ethan.g.907@gmail.com>
2017-02-26 20:10:13 -0500

    additions to share tools

--- new-new/scripts/main.js
+++ new-new/scripts/main.js
@@ -6,23 +6,56 @@ $(window).scroll( function() {
 	$('.site-header').removeClass('minimize');
 });
 
+$(document).ready(function() {
 
-// The clock JS
-var updateTime = function() {
-	var time = new Date(),
-			seconds = (time.getSeconds()/60)*360,
-			minutes = (time.getMinutes()/60)*360,
-			hour = (time.getHours()/12)*360;
+	var module = {};
 
-	var secondsHand = document.querySelector('.seconds'),
-			minutesHand = document.querySelector('.minutes'),
-			hourHand = document.querySelector('.hours');
+	module.init = function() {
+		module.activeClass = 'share-tools--active';
+		module.shareTools = document.querySelector('.js-share-tools');
+		module.shareToolsBtn = document.querySelector('.js-share-btn');
 
-	secondsHand.style.transform = 'rotate(' + seconds + 'deg)';
-	minutesHand.style.transform = 'rotate(' + minutes + 'deg)';
-	hourHand.style.transform = 'rotate(' + hour + 'deg)';
-};
+		module.updateClock();
+		module.eventHandlers();
+	}
 
-setInterval(function() {
-	updateTime();
-}, 1000);
+	module.eventHandlers = function() {
+		module.shareToolsBtn.addEventListener('mouseenter', module.toggleShareTools);
+		module.shareTools.addEventListener('mouseleave', module.toggleShareTools);
+	}
+
+	// Share tools JS
+	module.toggleShareTools = function() {
+		var classList = module.shareTools.classList
+		var hoveringFromSelf = event.fromElement == document.querySelector('.js-share-btns > *');
+		
+		if ( classList.contains(module.activeClass) && !hoveringFromSelf) {
+			classList.remove(module.activeClass);
+		} else {
+			classList.add(module.activeClass);
+		}
+	}
+
+	// The clock JS
+	module.updateClock = function() {
+		var time = new Date(),
+				seconds = (time.getSeconds()/60)*360,
+				minutes = (time.getMinutes()/60)*360,
+				hour = (time.getHours()/12)*360;
+
+		var secondsHand = document.querySelector('.seconds'),
+				minutesHand = document.querySelector('.minutes'),
+				hourHand = document.querySelector('.hours');
+
+		secondsHand.style.transform = 'rotate(' + seconds + 'deg)';
+		minutesHand.style.transform = 'rotate(' + minutes + 'deg)';
+		hourHand.style.transform = 'rotate(' + hour + 'deg)';
+	};
+
+	setInterval(function() {
+		module.updateClock();
+	}, 1000);
+
+	module.init();
+});
+
